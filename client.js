ccdeda112f87ae95daea5021e0e6e6be5533c8ba
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2021-12-02 18:59:22 -0500

    updated snake exercise

--- client.js
+++ client.js
@@ -2,6 +2,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
+const { IP, PORT } = require("./constants");
 const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
@@ -10,7 +11,9 @@ const connect = function() {
   
   conn.on("connect", () => {
     console.log('"Successfully connected to game server"');
+
     conn.write("Name: MN");
+    conn.write(" Say: HEY");
     
   
   });
@@ -21,8 +24,8 @@ const connect = function() {
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
+
+
 
 
 module.exports = {connect};
